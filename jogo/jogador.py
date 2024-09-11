234f6d538a3361f780f8ae9392f4e9376bfd0b1e
Hirata <95556536+n7Hirata@users.noreply.github.com>
2024-09-10 20:36:09 -0400

    refatoração

---
+++ jogo/jogador.py
@@ -0,0 +1,14 @@
+# funcao moverJogador() registra a posição do jogador
+def moverJogador(jogador, teclas, dim_janela):
+    borda_esquerda = 0
+    borda_superior = 0
+    borda_direita = dim_janela[0]
+    borda_inferior = dim_janela[1]
+    if teclas['esquerda'] and jogador['objRect'].left > borda_esquerda:
+        jogador['objRect'].x -= jogador['vel']
+    if teclas['direita'] and jogador['objRect'].right < borda_direita:
+        jogador['objRect'].x += jogador['vel']
+    if teclas['cima'] and jogador['objRect'].top > borda_superior:
+        jogador['objRect'].y -= jogador['vel']
+    if teclas['baixo'] and jogador['objRect'].bottom < borda_inferior:
+        jogador['objRect'].y += jogador['vel']
---
+++ jogo/som.py
@@ -0,0 +1,5 @@
+# configurando o som
+somEstrela = pygame.mixer.Sound('./resources/estrela.mp3')
+somMiado = pygame.mixer.Sound('./resources/miado.mp3')
+somEstrela.set_volume(0.1)
+somAtivado = True
